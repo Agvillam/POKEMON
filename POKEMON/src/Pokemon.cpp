fdfdade882f382317d9b01419d0d2a4b6a9c7b8b
Agvillam <87862630+Agvillam@users.noreply.github.com>
2023-07-12 19:16:49 +0200

    Adición de librería Freeglut

---
+++ POKEMON/src/Pokemon.cpp
@@ -0,0 +1,96 @@
+#include "freeglut.h"
+#include <math.h>
+
+void OnDraw(void); //esta funcion sera llamada para dibujar
+void OnTimer(int value); //esta funcion sera llamada cuando transcurra una temporizacion
+void OnKeyboardDown(unsigned char key, int x, int y); //cuando se pulse una tecla	
+
+int main(int argc, char* argv[])
+{
+	//Inicializar el gestor de ventanas GLUT
+	//y crear la ventana
+	glutInit(&argc, argv);
+	glutInitWindowSize(800, 600);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutCreateWindow("MiJuego");
+
+	//habilitar luces y definir perspectiva
+	glEnable(GL_LIGHT0);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_COLOR_MATERIAL);
+	glMatrixMode(GL_PROJECTION);
+	gluPerspective(40.0, 800 / 600.0f, 0.1, 150);
+
+	//Registrar los callbacks
+	glutDisplayFunc(OnDraw);
+	glutTimerFunc(25, OnTimer, 0);//le decimos que dentro de 25ms llame 1 vez a la funcion OnTimer()
+	glutKeyboardFunc(OnKeyboardDown);
+
+
+	//pasarle el control a GLUT,que llamara a los callbacks
+	glutMainLoop();
+
+	return 0;
+}
+
+void OnDraw(void)
+{
+	//Borrado de la pantalla	
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	//Para definir el punto de vista
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	glutSetColor(1,200,200,200);
+	glTranslated(0,0,0);
+	glutSolidCone(12.5,30,10,15);
+	
+	int x_ojo = 0;
+	int y_ojo = 10;
+	int z_ojo = 20;
+
+	float dist = sqrt(x_ojo * x_ojo + z_ojo * z_ojo);
+	float ang = atan2(z_ojo, x_ojo);
+	ang += 0.05f;
+	x_ojo = dist * cos(ang);
+	z_ojo = dist * sin(ang);
+
+	gluLookAt(x_ojo, y_ojo, z_ojo,  // posicion del ojo
+		0.0, 0, 0.0,      // hacia que punto mira  (0,0,0) 
+		0.0, 1.0, 0.0);      // definimos hacia arriba (eje Y)    
+
+	//aqui es donde hay que poner el codigo de dibujo
+	//dibujo del suelo
+	glDisable(GL_LIGHTING);
+	glBegin(GL_POLYGON);
+	glColor3ub(255, 0, 0);
+	glVertex3f(-5.0f, 0, -5.0f);
+	glVertex3f(-5.0f, 0, 5.0f);
+	glColor3ub(255, 255, 0);
+	glVertex3f(5.0f, 0, 5.0f);
+	glVertex3f(5.0f, 0, -5.0f);
+	glEnd();
+	glEnable(GL_LIGHTING);
+
+
+	//no borrar esta linea ni poner nada despues
+	glutSwapBuffers();
+}
+void OnKeyboardDown(unsigned char key, int x_t, int y_t)
+{
+	//poner aqui el código de teclado
+	
+
+	glutPostRedisplay();
+}
+
+void OnTimer(int value)
+{
+	//poner aqui el código de animacion
+
+	//no borrar estas lineas
+	glutTimerFunc(25, OnTimer, 0);
+	glutPostRedisplay();
+}
